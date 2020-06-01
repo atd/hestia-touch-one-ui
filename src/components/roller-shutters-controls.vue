c1ace177cf124b1ec39864cd077f736246f17932
Antonio Tapiador del Dujo <atd@singularities.org>
2020-06-01 22:47:48 +0200

    Change div names

--- src/components/roller-shutters-controls.vue
+++ src/components/roller-shutters-controls.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="grid roller-shutters-grid">
     <div class="roller-shutters-container">
-      <div class="roller-shutter roller-shutter-left"
+      <div class="roller-shutter-window roller-shutter-window-left"
         data-label="left"
         @click="update">
-        <div class="roller-shutter-holes" :style="{ height: shutterHeight('left')}"></div>
+        <div class="roller-shutter" :style="{ height: shutterHeight('left')}"></div>
       </div>
-      <div class="roller-shutter roller-shutter-middle"
+      <div class="roller-shutter-window roller-shutter-window-middle"
         data-label="middle"
         @click="update">
-        <div class="roller-shutter-holes" :style="{ height: shutterHeight('middle')}"></div>
+        <div class="roller-shutter" :style="{ height: shutterHeight('middle')}"></div>
       </div>
-      <div class="roller-shutter roller-shutter-right"
+      <div class="roller-shutter-window roller-shutter-window-right"
         data-label="right"
         @click="update">
-        <div class="roller-shutter-holes" :style="{ height: shutterHeight('right')}"></div>
+        <div class="roller-shutter" :style="{ height: shutterHeight('right')}"></div>
       </div>
     </div>
   </div>
@@ -51,27 +51,27 @@ export default {
     height: 100%;
   }
 
-  .roller-shutter {
+  .roller-shutter-window {
     position: absolute;
     height: 210px;
     background-color: #e0e5e8;
   }
 
-  .roller-shutter-left {
+  .roller-shutter-window-left {
     width: 105px;
   }
 
-  .roller-shutter-middle {
+  .roller-shutter-window-middle {
     width: 210px;
     left: 128px;
   }
 
-  .roller-shutter-right {
+  .roller-shutter-window-right {
     width: 105px;
     right: 3%;
   }
 
-  .roller-shutter-holes {
+  .roller-shutter {
     background-image: linear-gradient(to bottom, #000, #000 90%, transparent 90%, transparent 100%), linear-gradient(90deg, #000, #000 60%, #e0e5e8 60%, #e0e5e8 100%);
     background-size: 100% 20px, 20px 3px;
     transform: scaleY(-1);
