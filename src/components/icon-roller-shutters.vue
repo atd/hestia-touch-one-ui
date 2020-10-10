d590b6d005ec3132e4c2e423ff0ff39b5f4febdc
Antonio Tapiador del Dujo <atd@singularities.org>
2020-10-10 22:01:15 +0200

    Update roller shutter icon

--- src/components/icon-roller-shutters.vue
+++ src/components/icon-roller-shutters.vue
@@ -1,16 +1,146 @@
 <template>
   <svg
-    version="1.0"
-    xmlns="http://www.w3.org/2000/svg"
-    class="icon"
-    :width="size"
-    :height="size"
-    viewBox="0 0 692 802"
-    style="enable-background:new 0 0 611.999 611.999;"
-    stroke="currentColor"
-    fill="currentColor">
-      <path d="M17 67v47h656V20H17v47zM21.6 123.9c-.3.5 2.5 4.7 6.4 9.3l7 8.3V736H21v39h638v-39h-9V143.5l9-9c5-4.9 9-9.5 9-10.2 0-1.1-62.4-1.3-322.9-1.3-191.1 0-323.2.4-323.5.9zm607 35.8l.2 5.3H57v-11l285.7.2 285.7.3.2 5.2zm-.1 42.3v10.5l-285.7.3L57 213v-22l285.8.2 285.7.3V202zm0 48.5V262l-285.7.3-285.8.2v-24l285.8.2 285.7.3v11.5zm0 48.5v10.5l-285.7.3L57 310v-22l285.8.2 285.7.3V299zm0 49v11.5l-285.7.3L57 360v-24l285.8.2 285.7.3V348zm.3 48.2l-.3 10.3-285.7.3L57 407v-21h572.1l-.3 10.2zm-.3 48.8v11.5l-285.7.3L57 457v-24l285.8.2 285.7.3V445zm-.2 40.2c.3 1.3.5 6.1.4 10.8l-.2 8.5-285.7.2-285.8.1V483h570.8l.5 2.2zm.2 55.8v10.5l-285.7.3L57 552v-22l285.8.2 285.7.3V541zm.1 47.7l.1 10.3H57v-21l285.8.2 285.7.3.1 10.2zm-.1 48.5V649H57v-24l285.8.2 285.7.3v11.7zm-.2 47.3c-.1 4.9-.2 9.8-.2 10.7-.1 1.7-14.4 1.8-285.6 1.8H57v-22l285.7.2 285.7.3-.1 9zm.1 44.7l-.1 6.3-285.6.3L57 736v-13.5l285.7.2 285.7.3v6.2z"/>
-    </svg>
+     xmlns:dc="http://purl.org/dc/elements/1.1/"
+     xmlns:cc="http://creativecommons.org/ns#"
+     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+     xmlns:svg="http://www.w3.org/2000/svg"
+     xmlns="http://www.w3.org/2000/svg"
+     xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+     xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+     width="64.460381"
+     height="69.922073"
+     viewBox="0 0 17.055142 18.500217"
+     version="1.1"
+     id="svg8"
+     stroke="currentColor"
+     fill="currentColor"
+     class="icon"
+     inkscape:version="1.0.1 (3bc2e813f5, 2020-09-07)"
+     sodipodi:docname="icon.svg">
+    <defs
+       id="defs2" />
+    <sodipodi:namedview
+       id="base"
+       borderopacity="0"
+       inkscape:pageopacity="0.0"
+       inkscape:pageshadow="2"
+       inkscape:zoom="2.825798"
+       inkscape:cx="24.844212"
+       inkscape:cy="41.203372"
+       inkscape:document-units="mm"
+       inkscape:current-layer="layer1"
+       inkscape:document-rotation="0"
+       showgrid="false"
+       inkscape:window-width="1920"
+       inkscape:window-height="1016"
+       inkscape:window-x="0"
+       inkscape:window-y="27"
+       inkscape:window-maximized="1"
+       fit-margin-top="0"
+       fit-margin-left="0"
+       fit-margin-right="0"
+       fit-margin-bottom="0"
+       units="px" />
+    <metadata
+       id="metadata5">
+      <rdf:RDF>
+        <cc:Work
+           rdf:about="">
+          <dc:format>image/svg+xml</dc:format>
+          <dc:type
+             rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+          <dc:title></dc:title>
+        </cc:Work>
+      </rdf:RDF>
+    </metadata>
+    <g
+       inkscape:label="Capa 1"
+       inkscape:groupmode="layer"
+       id="layer1"
+       transform="translate(-75.812134,-39.961136)">
+      <g
+         id="g1702"
+         transform="matrix(0.91205987,0,0,0.79721938,6.6669309,8.1033441)">
+        <path
+           style="fill:none;fill-rule:evenodd;stroke-width:0.732626;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"
+           d="m 77.168085,52.735371 -0.02146,5.315541 14.302071,0.04302 -0.01105,-5.358557"
+           id="path993" />
+        <rect
+           style="fill-opacity:1;stroke-width:2.21876;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0"
+           id="rect995"
+           width="16.055141"
+           height="3.8156884"
+           x="75.812134"
+           y="39.961136" />
+        <g
+           id="g1650"
+           transform="translate(6.5287235,7.246118)">
+          <rect
+             style="fill-opacity:1;stroke-width:0.0688177"
+             id="rect833-75"
+             width="14.983204"
+             height="1.2027968"
+             x="70.282829"
+             y="36.484978" />
+          <rect
+             style="fill-opacity:1;stroke-width:0.0648699"
+             id="rect833-6-8"
+             width="14.983204"
+             height="1.0687587"
+             x="70.283562"
+             y="38.497124" />
+          <path
+             style="fill:none;fill-rule:evenodd;stroke-width:1.665;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:1.665, 1.665;stroke-dashoffset:0;stroke-opacity:1"
+             d="M 70.282829,37.687775 H 85.266037"
+             id="path1634" />
+        </g>
+        <g
+           id="g1650-2"
+           transform="matrix(1,0,0,1.0257521,6.5294559,9.3081178)">
+          <rect
+             style="fill-opacity:1;stroke-width:0.0688177"
+             id="rect833-75-9"
+             width="14.983204"
+             height="1.2027968"
+             x="70.282829"
+             y="36.484978" />
+          <rect
+             style="fill-opacity:1;stroke-width:0.0648699"
+             id="rect833-6-8-3"
+             width="14.983204"
+             height="1.0687587"
+             x="70.283562"
+             y="38.497124" />
+          <path
+             style="fill:none;fill-rule:evenodd;stroke-width:1.665;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:1.665, 1.665;stroke-dashoffset:0;stroke-opacity:1"
+             d="M 70.282829,37.687775 H 85.266037"
+             id="path1634-1" />
+        </g>
+        <g
+           id="g1650-9"
+           transform="matrix(1,0,0,1.014293,6.5301883,12.842415)">
+          <rect
+             style="fill-opacity:1;stroke-width:0.0688177"
+             id="rect833-75-4"
+             width="14.983204"
+             height="1.2027968"
+             x="70.282829"
+             y="36.484978" />
+          <rect
+             style="fill-opacity:1;stroke-width:0.0648699"
+             id="rect833-6-8-7"
+             width="14.983204"
+             height="1.0687587"
+             x="70.283562"
+             y="38.497124" />
+          <path
+             style="fill:none;fill-rule:evenodd;stroke-width:1.665;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:1.665, 1.665;stroke-dashoffset:0;stroke-opacity:1"
+             d="M 70.282829,37.687775 H 85.266037"
+             id="path1634-8" />
+        </g>
+      </g>
+    </g>
+  </svg>
 </template>
 
 <script>
